ca607b75ff6d6e7bda2a878a62282e4d57aa2635
gadam15 <adam.wrobel2004@gmail.com>
2024-02-05 16:36:40 +0100

    Added login and registration

--- src/login.jsx
+++ src/login.jsx
@@ -1,20 +1,50 @@
-import React from "react";
+import axios from "./Test/axios";
+import React, { Fragment, useState } from "react";
 import Banner from "./Components/banner";
 import Bottom from "./Components/bottom";
 
 const Login = () => {
+
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleEmailChange = (value) =>{
+        setPassword(value);
+    }
+
+    const handlePasswordChange = (value) =>{
+        setEmail(value);
+    }
+    
+    const handleLogin = () => {
+        const data = {
+            password : password,
+            email : email,
+        }
+        
+        const url = 'https://localhost:7107/api/UserAPI/Login'; 
+        
+        axios.post(url,data).then((result)=>{
+            alert(result.data);
+        }).catch((error)=>{
+            alert(error.response.data);
+        })
+    }
+
     return(
+        <Fragment>
         <div className="logowanie">
             <Banner/>
             <div className="zaloguj">
-                <h2>Nazwa użytkownika:</h2>
-                <input type="text"/>
+                <h2>Email</h2>
+                <input type="text" onChange={(e) => handleEmailChange(e.target.value)}/>
                 <h2>Hasło:</h2>
-                <input type="password"/><br/>
-                <input type="button" value="Zaloguj"/>
+                <input type="password" onChange={(e) => handlePasswordChange(e.target.value)}/><br/>
+                <input type="button" onClick={handleLogin} value="Zaloguj"/>
             </div>
             <Bottom/>
         </div>
+        </Fragment>
     );
 };
 
